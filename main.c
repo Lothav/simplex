097a623428cd98293d2535ad8dc03633e4400310
Luiz0tavio <me@luizotav.io>
2018-04-10 08:54:43 -0300

    open file improve - Fraction and Matrix structs

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
+struct Fraction {
+    int numerator;
+    int denominator;
+};
+
+struct Matrix {
+    struct Fraction **number;
+    int width;
+    int length;
+};
+
 int main(int argc, char* argv[])
 {
-    FILE* file = NULL;
-
-    if (argc == 1) {
-        file = fopen(stdin, "r");
-    } else {
-        file = fopen(argv[1], "r");
-    }
-
+    FILE* file = fopen(argc == 1 ? stdin : argv[1], "r");
 
     if (NULL == file) {
-        printf("Error opening input file!\n");
+        printf("Error opening input file!\nIt can be pass as param or stdin\n");
         return 1;
     }
-
+    
     fclose(file);
     return 0;
 }
