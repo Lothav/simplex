263a720af66b75b4435d21f26a9485e95037747b
Luiz0tavio <me@luizotav.io>
2018-05-05 13:45:09 -0300

    getMatrixDataFromString method

--- main.cpp
+++ main.cpp
@@ -4,55 +4,9 @@
 
 #include <iostream>
 #include <fstream>
-#include <regex.h>
 #include <cstring>
 #include <vector>
-
-char** regexRow(char* src, unsigned int row_size)
-{
-    int res, len;
-    auto *result = (char *)malloc(BUFSIZ);
-    auto **return_value = (char **)malloc(row_size * sizeof(char *));
-    char err_buf[BUFSIZ];
-    char *aux = src;
-
-    const char* pattern = "\\[((\\-*[0-9]+[\\,\\ ]*)+)\\]";
-
-    regex_t preg = {};
-    regmatch_t pmatch[10];
-
-    if( (res = regcomp(&preg, pattern, REG_EXTENDED)) != 0)
-    {
-        regerror(res, &preg, err_buf, BUFSIZ);
-        printf("regcomp() error: %s\n", err_buf);
-        exit(res);
-    }
-
-    long offset = 0;
-    int i;
-    for(i = 0; i < row_size; i++)
-    {
-        aux += offset;
-        res = regexec(&preg, aux, 10, pmatch, REG_NOTBOL);
-        if(res == REG_NOMATCH) break;
-
-        offset = pmatch[0].rm_eo - pmatch[0].rm_so;
-
-        len = pmatch[1].rm_eo - pmatch[1].rm_so;
-        memcpy(result, aux + pmatch[1].rm_so, (size_t)len);
-        result[len] = '\0';
-
-        return_value[i] = (char *)malloc((size_t)len + 1);
-        memcpy(return_value, result, (size_t)len);
-        printf("%s\n", result);
-    }
-
-    regfree(&preg);
-    free(result);
-
-    return return_value;
-}
-
+#include <regex>
 
 std::vector<std::string> readStream(std::istream& input_stream)
 {
@@ -90,6 +44,21 @@ std::vector<std::string> buildFromStdIn()
     return readStream(std::cin);
 }
 
+std::vector<long> getMatrixDataFromString(std::string matrix_data_string)
+{
+    std::vector<long> matrix_data_long = {};
+
+    std::regex r("(\\+|-)?[[:digit:]]+");
+    std::smatch m;
+
+    while (std::regex_search(matrix_data_string, m, r)) {
+        matrix_data_long.push_back( std::stoi (m[0]) );
+        matrix_data_string = m.suffix().str();
+    }
+
+    return matrix_data_long;
+}
+
 int main(int argc, char** argv)
 {
     std::vector<std::string> file_data;
@@ -99,7 +68,7 @@ int main(int argc, char** argv)
         file_data = buildFromStdIn();
     }
 
-    if (file_data.size() == 3) {
+    if (file_data.size() != 3) {
         std::cerr << "Invalid file lines!" << std::endl;
         std::cerr << "File must have 3 lines:" << std::endl;
         std::cerr << "\tm+1" << std::endl;
@@ -108,7 +77,7 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    std::cout << file_data.size() << std::endl;
+    auto matrix_data = getMatrixDataFromString(file_data[2]);
 
     return EXIT_SUCCESS;
 }
