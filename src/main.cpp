04f4ac421c38b90c91b4846f9f9381c155e495e1
Luiz0tavio <me@luizotav.io>
2018-09-27 11:16:02 -0300

    properly handle file inputs

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,36 @@
 #include <fstream>
 #include <cstring>
 #include <vector>
-#include <regex>
 #include <climits>
 #include "Simplex/Tableaux.hpp"
 
-#define SOLUTION_WRITE_FILE "conclusao.txt"
-#define STEP_WRITE_FILE "pivoteamento.txt"
+#define STEPS_WRITE_FILE "steps.txt"
 
 int main(int argc, char** argv)
 {
+    if (argc != 3) {
+        std::cerr << "Wrong number of parameters!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string input_file  = argv[1];
+    std::string output_file = argv[2];
+
     const clock_t begin_time = std::clock();
     // Tableaux Unique_Ptr scope.
     {
         // Generate matrix from input file.
-        auto tableaux = std::make_unique<Simplex::Tableaux>(
-#ifdef INTERFACE_COMPLETE
-            interfaceComplete(argc, argv)
-#else
-            Simplex::File::GetTableauxInputFromFile(argv[1])
-#endif
-        );
-        tableaux->solve(STEP_WRITE_FILE);
-        auto solution = tableaux->getOutput();
-        Simplex::File::WriteSolution(SOLUTION_WRITE_FILE, solution);
+        auto tableaux = std::make_unique<Simplex::Tableaux>(Simplex::File::GetTableauxInputFromFile(std::move(input_file)));
+        tableaux->solve(STEPS_WRITE_FILE);
 
+        auto solution = tableaux->getOutput();
+        Simplex::File::WriteSolution(output_file, solution);
     }
     const clock_t end_time = std::clock();
     auto time_spent = static_cast<float>( end_time - begin_time) / CLOCKS_PER_SEC;
     std::cout << "Finished. Took " << time_spent << "s." << std::endl;
-    std::cout << "\tThe results was written in '" << SOLUTION_WRITE_FILE << "'." << std::endl;
-    std::cout << "\tMatrix steps was written in '" << STEP_WRITE_FILE << "'." << std::endl;
+    std::cout << "\tThe results was written in '" << output_file << "'." << std::endl;
+    std::cout << "\tMatrix steps was written in '" << STEPS_WRITE_FILE << "'." << std::endl;
 
     return EXIT_SUCCESS;
 }
