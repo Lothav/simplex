355681398d142e4a3bafe25d160daa4dd36e6de0
Ronald Pereira <ronaldpereira@users.noreply.github.com>
2019-10-01 07:57:06 -0300

    using namespace std inserted

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,34 @@
 #include "Simplex/Tableaux.hpp"
 
 #define STEPS_WRITE_FILE "steps.txt"
+using namespace std;
 
 int main(int argc, char** argv)
 {
     if (argc != 3) {
-        std::cerr << "Wrong number of parameters!" << std::endl;
+        cerr << "Wrong number of parameters!" << endl;
         return EXIT_FAILURE;
     }
 
-    std::string input_file  = argv[1];
-    std::string output_file = argv[2];
+    string input_file  = argv[1];
+    string output_file = argv[2];
 
     const clock_t begin_time = std::clock();
     // Tableaux Unique_Ptr scope.
     {
         // Generate matrix from input file.
-        auto tableaux_input = Simplex::File::GetTableauxInputFromFile(std::move(input_file));
-        auto tableaux = std::make_unique<Simplex::Tableaux>(std::move(tableaux_input));
+        auto tableaux_input = Simplex::File::GetTableauxInputFromFile(move(input_file));
+        auto tableaux = make_unique<Simplex::Tableaux>(move(tableaux_input));
         tableaux->solve(STEPS_WRITE_FILE);
 
         auto solution = tableaux->getOutput();
         Simplex::File::WriteSolution(output_file, solution);
     }
-    const clock_t end_time = std::clock();
+    const clock_t end_time = clock();
     auto time_spent = static_cast<float>(end_time - begin_time) / CLOCKS_PER_SEC;
-    std::cout << "Finished. Took " << time_spent << "s." << std::endl;
-    std::cout << "\tThe results was written in '" << output_file << "'." << std::endl;
-    std::cout << "\tMatrix steps was written in '" << STEPS_WRITE_FILE << "'." << std::endl;
+    cout << "Finished. Took " << time_spent << "s." << std::endl;
+    cout << "\tThe results was written in '" << output_file << "'." << endl;
+    cout << "\tMatrix steps was written in '" << STEPS_WRITE_FILE << "'." << endl;
 
     return EXIT_SUCCESS;
 }
